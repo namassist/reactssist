189de910c61e31bc472727ab5a77952c8badb623
namassist <chairulanmm@gmail.com>
2021-09-08 19:14:30 +0700

    menyingkat code

--- Tugas-Harian-Part-1/Tugas-8/index3.js
+++ Tugas-Harian-Part-1/Tugas-8/index3.js
@@ -6,27 +6,19 @@ filterBooksPromise(true, 40)
     console.log(data);
   })
   .catch(function (err) {
-    console.log(err);
+    console.log(err.message);
   });
 
 // bukunya tidak berwarna dan jumlah halamannya 250 (gunakan async/await untuk kondisi ini)
-async function filterBooksUncolorfull() {
+async function filterBooks() {
   try {
-    const books = await filterBooksPromise(false, 250);
-    console.log(books);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-async function filterBooksColorfull() {
-  try {
-    const books = await filterBooksPromise(true, 30);
-    console.log(books);
+    const booksUncolorfull = await filterBooksPromise(false, 250);
+    const booksColorfull = await filterBooksPromise(true, 30);
+    console.log(booksUncolorfull);
+    console.log(booksColorfull);
   } catch (err) {
     console.log(err.message);
   }
 }
 
-filterBooksUncolorfull();
-filterBooksColorfull();
+filterBooks();
